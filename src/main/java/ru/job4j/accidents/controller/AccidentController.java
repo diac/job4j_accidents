b7daf58c64f4e54ade7881596b9af7848bd94e8a
diac <nikolai.gladkikh@gmail.com>
2022-12-20 12:53:09 +0300

    3. @ModelAttribute. Создание инцидента. [#261013]

---
+++ src/main/java/ru/job4j/accidents/controller/AccidentController.java
@@ -0,0 +1,52 @@
+package ru.job4j.accidents.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.service.AccidentService;
+
+import java.util.Optional;
+
+@Controller
+@AllArgsConstructor
+public class AccidentController {
+
+    private AccidentService accidentService;
+
+    @GetMapping("/accidents")
+    public String index(Model model) {
+        model.addAttribute("user", "Petr Arsentev");
+        model.addAttribute("accidents", accidentService.findAll());
+        return "index";
+    }
+
+    @GetMapping("/addAccident")
+    public String create(Model model) {
+        return "createAccident";
+    }
+
+    @PostMapping("/saveAccident")
+    public String store(@ModelAttribute Accident accident) {
+        accidentService.add(accident);
+        return "redirect:/accidents";
+    }
+
+    @GetMapping("/accidents/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        Optional<Accident> accident = accidentService.findById(id);
+        if (accident.isEmpty()) {
+            return "redirect:/accidents";
+        }
+        model.addAttribute("accident", accident.get());
+        return "editAccident";
+    }
+
+    @PatchMapping("/accidents/{id}")
+    public String patch(@PathVariable("id") int id, @ModelAttribute("accident") Accident accident) {
+        accident.setId(id);
+        accidentService.update(accident);
+        return "redirect:/accidents";
+    }
+}
--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.service.AccidentService;
 
 @Controller
@@ -15,11 +14,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
-        for (int i = 1; i <= 5; i++) {
-            String name = "Нарушение №" + i;
-            String address = "Адрес №" + i;
-            accidentService.add(new Accident(0, name, name, address));
-        }
         model.addAttribute("user", "Petr Arsentev");
         model.addAttribute("accidents", accidentService.findAll());
         return "index";
