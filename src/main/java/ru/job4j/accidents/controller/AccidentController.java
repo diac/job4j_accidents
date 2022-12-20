0afec5d6ad1c1071200ded708c2665d60257b9c1
diac <nikolai.gladkikh@gmail.com>
2022-12-20 15:58:56 +0300

    5. Form с агрегационными объектами [#305523]

--- src/main/java/ru/job4j/accidents/controller/AccidentController.java
+++ src/main/java/ru/job4j/accidents/controller/AccidentController.java
@@ -6,11 +6,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.model.AccidentType;
+import ru.job4j.accidents.model.Rule;
 import ru.job4j.accidents.service.AccidentService;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Controller
 @AllArgsConstructor
@@ -32,12 +33,28 @@ public class AccidentController {
         types.add(new AccidentType(2, "Машина и человек"));
         types.add(new AccidentType(3, "Машина и велосипед"));
         model.addAttribute("types", types);
+        List<Rule> rules = List.of(
+                new Rule(1, "Статья. 1"),
+                new Rule(2, "Статья. 2"),
+                new Rule(3, "Статья. 3")
+        );
+        model.addAttribute("rules", rules);
         return "createAccident";
     }
 
     @PostMapping("/saveAccident")
-    public String store(@ModelAttribute Accident accident, @RequestParam("type.id") int typeId) {
+    public String store(
+            @ModelAttribute Accident accident,
+            @RequestParam("type.id") int typeId,
+            HttpServletRequest req
+    ) {
         accident.setType(new AccidentType(typeId, null));
+        String[] ruleIds = Optional.ofNullable(req.getParameterValues("rIds")).orElse(new String[0]);
+        accident.setRules(
+                Arrays.stream(ruleIds)
+                        .map(id -> new Rule(Integer.parseInt(id), null))
+                        .collect(Collectors.toSet())
+        );
         accidentService.add(accident);
         return "redirect:/accidents";
     }
@@ -54,11 +71,20 @@ public class AccidentController {
         types.add(new AccidentType(2, "Машина и человек"));
         types.add(new AccidentType(3, "Машина и велосипед"));
         model.addAttribute("types", types);
+        List<Rule> rules = List.of(
+                new Rule(1, "Статья. 1"),
+                new Rule(2, "Статья. 2"),
+                new Rule(3, "Статья. 3")
+        );
+        model.addAttribute("rules", rules);
         return "editAccident";
     }
 
     @PatchMapping("/accidents/{id}")
-    public String patch(@PathVariable("id") int id, @ModelAttribute("accident") Accident accident) {
+    public String patch(
+            @PathVariable("id") int id,
+            @ModelAttribute("accident") Accident accident
+    ) {
         accident.setId(id);
         accidentService.update(accident);
         return "redirect:/accidents";
