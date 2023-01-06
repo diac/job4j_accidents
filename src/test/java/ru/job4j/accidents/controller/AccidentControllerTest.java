2cc13a9c7ac8941406b4763d5d773a9328fa192b
diac <nikolai.gladkikh@gmail.com>
2023-01-06 13:59:29 +0300

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/accidents/controller/AccidentControllerTest.java
+++ src/test/java/ru/job4j/accidents/controller/AccidentControllerTest.java
@@ -1,9 +1,11 @@
 package ru.job4j.accidents.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.accidents.Main;
@@ -13,8 +15,12 @@ import ru.job4j.accidents.service.AccidentService;
 import ru.job4j.accidents.service.AccidentTypeService;
 
 import java.util.HashSet;
+import java.util.Optional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -26,10 +32,10 @@ public class AccidentControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
+    @MockBean
     private AccidentService accidentService;
 
-    @Autowired
+    @MockBean
     private AccidentTypeService accidentTypeService;
 
     @Test
@@ -53,18 +59,54 @@ public class AccidentControllerTest {
     @Test
     @WithMockUser
     public void whenEditAccidentView() throws Exception {
-        AccidentType accidentType = accidentTypeService.add(new AccidentType(0, "Test"))
+        String value = String.valueOf(System.currentTimeMillis());
+        AccidentType accidentType = accidentTypeService.add(new AccidentType(0, value))
                 .orElse(new AccidentType());
-        Accident accident = accidentService.add(
-                        new Accident(0, "Test", "Test", "Test", accidentType, new HashSet<>())
-                )
-                .orElse(new Accident());
+        Accident accident = new Accident(0, value, value, value, accidentType, new HashSet<>());
+        when(accidentService.findById(0)).thenReturn(Optional.of(accident));
         mockMvc.perform(
                         get("/accidents/edit")
-                                .param("id", String.valueOf(accident.getId()))
+                                .param("id", "0")
                 )
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("editAccident"));
+    }
+
+    @Test
+    @WithMockUser
+    public void whenStoreAccidentThenRedirect() throws Exception {
+        String value = String.valueOf(System.currentTimeMillis());
+        AccidentType accidentType = accidentTypeService.add(new AccidentType(0, value))
+                .orElse(new AccidentType());
+        Accident accident = new Accident(0, value, value, value, accidentType, new HashSet<>());
+        mockMvc.perform(
+                        post("/saveAccident")
+                                .param("type.id", String.valueOf(accidentType.getId()))
+                                .flashAttr("accident", accident)
+                )
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Accident> argumentCaptor = ArgumentCaptor.forClass(Accident.class);
+        verify(accidentService).add(argumentCaptor.capture());
+        assertThat(argumentCaptor.getValue().getName()).isEqualTo(accident.getName());
+    }
+
+    @Test
+    @WithMockUser
+    public void whenPatchAccidentThenRedirect() throws Exception {
+        String value = String.valueOf(System.currentTimeMillis());
+        AccidentType accidentType = accidentTypeService.add(new AccidentType(0, value))
+                .orElse(new AccidentType());
+        Accident accident = new Accident(0, value, value, value, accidentType, new HashSet<>());
+        mockMvc.perform(
+                patch("/accidents/{id}", 0)
+                        .flashAttr("accident", accident)
+        )
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Accident> argumentCaptor = ArgumentCaptor.forClass(Accident.class);
+        verify(accidentService).update(argumentCaptor.capture());
+        assertThat(argumentCaptor.getValue().getName()).isEqualTo(accident.getName());
     }
 }
--- src/test/java/ru/job4j/accidents/controller/RegistrationControllerTest.java
+++ src/test/java/ru/job4j/accidents/controller/RegistrationControllerTest.java
@@ -1,13 +1,22 @@
 package ru.job4j.accidents.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.accidents.Main;
+import ru.job4j.accidents.model.Authority;
+import ru.job4j.accidents.model.User;
+import ru.job4j.accidents.repository.AuthorityRepository;
+import ru.job4j.accidents.repository.UserRepository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -19,11 +28,33 @@ public class RegistrationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private AuthorityRepository authorityRepository;
+
     @Test
     public void whenRegistrationView() throws Exception {
         mockMvc.perform(get("/registration"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("registration"));
+    }
+
+    @Test
+    public void whenRegistrationSubmit() throws Exception {
+        String value = String.valueOf(System.currentTimeMillis());
+        Authority authority = new Authority(0, value);
+        User user = new User(0, value, value, authority, true);
+        mockMvc.perform(
+                        post("/registration")
+                                .flashAttr("user", user)
+                )
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<User> argumentCaptor = ArgumentCaptor.forClass(User.class);
+        verify(userRepository).save(argumentCaptor.capture());
+        assertThat(argumentCaptor.getValue().getUsername()).isEqualTo(user.getUsername());
     }
 }
